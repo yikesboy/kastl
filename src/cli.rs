99e6035e93cb7f4622feba33d923058a78c5023c
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-03 23:39:24 +0200

    feat: set up basic Cli

---
+++ src/cli.rs
@@ -0,0 +1,16 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Test {
+        #[arg(short, long)]
+        list: bool,
+    },
+}
