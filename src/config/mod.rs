b43b091ffeb804505c764fc962c472c7d8ef3fc4
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-05 19:11:55 +0200

    refactor: updated Config -> ConfigManager and improved implementation

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,78 +1,65 @@
 mod error;
+pub mod secret;
+pub mod storage;
 
 use error::ConfigError;
-use keyring::Entry;
+use secret::SecretStorage;
 use serde::{Deserialize, Serialize};
-use std::fs::{create_dir_all, read_to_string, remove_file, write};
+use storage::ConfigStorage;
+use strum;
 
-const SERVICE_IDENTIFIER: &str = "kastl-cli";
-const CONFIG_FILE_NAME: &str = "config.toml";
+pub(crate) const SERVICE_IDENTIFIER: &str = "kastl-cli";
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub internal_url: String,
     pub username: String,
 }
 
-impl Config {
-    pub fn new(internal_url: String, username: String) -> Self {
+#[derive(Debug, strum::Display, strum::EnumIter)]
+#[strum(serialize_all = "snake_case")]
+pub enum SecretIdentifier {
+    Password,
+}
+
+#[derive(Debug)]
+pub struct Secret {
+    pub identifier: SecretIdentifier,
+    pub value: String,
+}
+
+pub struct ConfigManager<S: SecretStorage, C: ConfigStorage> {
+    secret_storage: S,
+    config_storage: C,
+}
+
+impl<S: SecretStorage, C: ConfigStorage> ConfigManager<S, C> {
+    pub fn new(secret_storage: S, config_storage: C) -> Self {
         Self {
-            internal_url,
-            username,
+            secret_storage,
+            config_storage,
         }
     }
 
-    pub fn save_config(&self) -> Result<(), ConfigError> {
-        let file_content = toml::to_string(self).map_err(ConfigError::UnableToConvertToToml)?;
-
-        let config_path = dirs::config_dir()
-            .ok_or(ConfigError::NoConfigDir)?
-            .join(SERVICE_IDENTIFIER);
-        create_dir_all(&config_path)?;
-
-        let config_file = config_path.join(CONFIG_FILE_NAME);
-        write(&config_file, file_content)?;
-
-        Ok(())
+    pub fn save_config(&self, config: &Config) -> Result<(), ConfigError> {
+        self.config_storage.save(&config)
     }
 
-    pub fn load_config() -> Result<Self, ConfigError> {
-        let config_path = dirs::config_dir()
-            .ok_or(ConfigError::NoConfigDir)?
-            .join(SERVICE_IDENTIFIER);
-        let config_file = config_path.join(CONFIG_FILE_NAME);
-        let raw_file_content = read_to_string(config_file)?;
-        toml::from_str(&raw_file_content).map_err(ConfigError::UnableToConvertFromToml)
+    pub fn load_config(&self) -> Result<Config, ConfigError> {
+        self.config_storage.load()
     }
 
-    pub fn save_password(&self, password: &str) -> Result<(), ConfigError> {
-        Entry::new(SERVICE_IDENTIFIER, &self.username)?.set_password(password)?;
-        Ok(())
+    pub fn save_secret(&self, secret: &Secret) -> Result<(), ConfigError> {
+        self.secret_storage.save(secret)
     }
 
-    pub fn load_password(&self) -> Result<String, ConfigError> {
-        let password = Entry::new(SERVICE_IDENTIFIER, &self.username)?.get_password()?;
-        Ok(password)
+    pub fn load_secret(&self, secret_identifier: SecretIdentifier) -> Result<Secret, ConfigError> {
+        self.secret_storage.load(secret_identifier)
     }
 
     pub fn delete_all(&self) -> Result<(), ConfigError> {
-        match Entry::new(SERVICE_IDENTIFIER, &self.username)?.delete_credential() {
-            Ok(_) => {}
-            Err(keyring::Error::NoEntry) => {}
-            Err(e) => return Err(ConfigError::Keyring(e)),
-        }
-
-        let config_file = dirs::config_dir()
-            .ok_or(ConfigError::NoConfigDir)?
-            .join(SERVICE_IDENTIFIER)
-            .join(CONFIG_FILE_NAME);
-
-        match remove_file(config_file) {
-            Ok(_) => {}
-            Err(e) if e.kind() == std::io::ErrorKind::NotFound => {}
-            Err(e) => return Err(ConfigError::Io(e)),
-        }
-
+        self.secret_storage.delete_all()?;
+        self.config_storage.delete()?;
         Ok(())
     }
 }
