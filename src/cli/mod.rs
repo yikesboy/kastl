a59b40dfb890fc4df468de30f712dfd3fdfdedd2
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-06 11:40:32 +0200

    feat: defined Auth and Entity commands

---
+++ src/cli/mod.rs
@@ -0,0 +1,48 @@
+mod auth;
+mod error;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Auth {
+        #[command(subcommand)]
+        command: AuthCommands,
+    },
+    Entities {
+        #[command(subcommand)]
+        command: EntityCommands,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+pub enum AuthCommands {
+    Login,
+    Logout,
+    Status,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum EntityCommands {
+    List {
+        #[arg(long)]
+        domain: Option<String>,
+    },
+    Get {
+        entity_id: String,
+    },
+    Set {
+        entity_id: String,
+        state: String,
+    },
+    // Delete {
+    //     entity_id: String,
+    // },
+}
