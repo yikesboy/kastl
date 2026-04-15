cb8abba9e0b3d19d027cbe1b6cc3bc0c1e582819
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-15 17:46:50 +0200

    feat: extended ha api wrapper with new routes and helpers

--- src/ha/mod.rs
+++ src/ha/mod.rs
@@ -1,16 +1,24 @@
-mod rest;
 pub mod error;
 mod model;
+mod rest;
 mod routes;
 
-use rest::HaRestClient;
+use chrono::{DateTime, Utc};
 use error::HaError;
+use rest::HaRestClient;
 
-use crate::{config::ConfigProvider, ha::model::{HaConfig, HaStatusMessage}};
 use crate::config::SecretIdentifier;
+use crate::ha::model::StateObject;
+use crate::{
+    config::ConfigProvider,
+    ha::model::{
+        Components, Events, HaConfig, HaStatusMessage, HistoryOptions, HistoryResponse,
+        LogbookOptions, LogbookResponse, Services, StatesResponse,
+    },
+};
 
 pub struct HaClient {
-    rest: HaRestClient
+    rest: HaRestClient,
 }
 
 impl HaClient {
@@ -22,7 +30,9 @@ impl HaClient {
 
     pub async fn from_config(config_provider: &impl ConfigProvider) -> Result<Self, HaError> {
         let config = config_provider.load_config()?;
-        let token = config_provider.load_secret(SecretIdentifier::BearerToken).await?;
+        let token = config_provider
+            .load_secret(SecretIdentifier::BearerToken)
+            .await?;
 
         Ok(Self::new(config.internal_url, token.value))
     }
@@ -33,5 +43,79 @@ impl HaClient {
 
     pub async fn get_config(&self) -> Result<HaConfig, HaError> {
         self.rest.get_config().await
+    }
+
+    pub async fn get_components(&self) -> Result<Components, HaError> {
+        self.rest.get_components().await
+    }
+
+    pub async fn get_events(&self) -> Result<Events, HaError> {
+        self.rest.get_events().await
+    }
+
+    pub async fn get_services(&self) -> Result<Services, HaError> {
+        self.rest.get_services().await
+    }
+
+    pub async fn get_history(
+        &self,
+        from: Option<DateTime<Utc>>,
+        query: Option<&HistoryOptions>,
+        for_entities: Vec<String>,
+    ) -> Result<HistoryResponse, HaError> {
+        self.rest.get_history(from, query, for_entities).await
+    }
+
+    pub async fn get_histroy_from_to_timestamp(
+        &self,
+        from: DateTime<Utc>,
+        to: DateTime<Utc>,
+        for_entities: Vec<String>,
+    ) -> Result<HistoryResponse, HaError> {
+        let options = HistoryOptions {
+            end_time: Some(to),
+            minimal_response: false,
+            no_attributes: false,
+            significant_changes_only: false,
+        };
+        self.rest
+            .get_history(Some(from), Some(&options), for_entities)
+            .await
+    }
+
+    pub async fn get_logbook(
+        &self,
+        from: Option<DateTime<Utc>>,
+        query: Option<&LogbookOptions>,
+    ) -> Result<LogbookResponse, HaError> {
+        self.rest.get_logbook(from, query).await
+    }
+
+    pub async fn get_logbook_from_to_timestamp(
+        &self,
+        from: DateTime<Utc>,
+        to: DateTime<Utc>,
+    ) -> Result<LogbookResponse, HaError> {
+        let options = LogbookOptions {
+            entity: None,
+            end_time: Some(to),
+        };
+        self.rest.get_logbook(Some(from), Some(&options)).await
+    }
+
+    pub async fn get_entity_logbook(&self, entity_id: String) -> Result<LogbookResponse, HaError> {
+        let options = LogbookOptions {
+            entity: Some(entity_id),
+            end_time: None,
+        };
+        self.rest.get_logbook(None, Some(&options)).await
+    }
+
+    pub async fn get_states(&self) -> Result<StatesResponse, HaError> {
+        self.rest.get_states().await
+    }
+
+    pub async fn get_entity_state(&self, entity_id: String) -> Result<StateObject, HaError> {
+        self.rest.get_entity_state(entity_id).await
     }
 }
