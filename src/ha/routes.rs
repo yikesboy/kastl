939384c351e2d0b158cd879091c2fee6b207c906
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-15 17:45:52 +0200

    feat: extended route constants by: COMPONENTS, EVENTS, SERVICES, HISTORY, LOGBOOK

--- src/ha/routes.rs
+++ src/ha/routes.rs
@@ -1,2 +1,7 @@
 pub const CONFIG: &str = "config";
+pub const COMPONENTS: &str = "components";
+pub const EVENTS: &str = "events";
+pub const SERVICES: &str = "services";
+pub const HISTORY: &str = "history/period";
+pub const LOGBOOK: &str = "logbook";
 pub const STATES: &str = "states";
