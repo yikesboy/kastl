4cc3bba85a6bc90deb192f947b783036a6886627
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-15 18:10:06 +0200

    feat: using tokio runtime and placeholder entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 mod cli;
+mod ha;
+mod config;
 
 use clap::Parser;
 use cli::Cli;
 
-fn main() {
+use crate::{cli::error::AppError, config::{ConfigService, secret::KeyringSecretStorage, storage::{ConfigStorage, FsConfigStorage}}};
+
+#[tokio::main]
+async fn main() -> Result<(), AppError> {
     let cli = Cli::parse();
     println!("{:?}", cli.command);
+
+    let config_manager = ConfigService::new(KeyringSecretStorage, FsConfigStorage);
+
+    match cli.command {
+        Some(command) => {
+            match command {
+                cli::Commands::Auth { command } => cli::auth::handle(command, &config_manager).await?,
+                cli::Commands::Entities { command } => unreachable!(),
+            }
+        },
+        None => unreachable!(),
+    }
+
+    Ok(())
 }
