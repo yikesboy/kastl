3229132bdfefa1c98a287291a6cd6363e1a2a9ca
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-15 17:46:27 +0200

    build: enabled feature "serde" for chrono and "derive" for serde

--- src/ha/rest.rs
+++ src/ha/rest.rs
@@ -1,6 +1,7 @@
 use crate::ha::error::HaError;
-use crate::ha::model::{HaConfig, HaStatusMessage};
-use crate::ha::routes::{CONFIG};
+use crate::ha::model::{Components, Events, HaConfig, HaStatusMessage, HistoryOptions, HistoryQuery, HistoryResponse, LogbookOptions, LogbookResponse, Services, StateObject, StatesResponse};
+use crate::ha::routes::{COMPONENTS, CONFIG, EVENTS, HISTORY, LOGBOOK, SERVICES, STATES};
+use chrono::{DateTime, Utc};
 use reqwest::{Client, StatusCode};
 use serde::de::DeserializeOwned;
 use serde::ser::Serialize;
@@ -14,11 +15,60 @@ pub struct HaRestClient {
 
 impl HaRestClient {
     pub async fn api_status(&self) -> Result<HaStatusMessage, HaError> {
-        self.get::<HaStatusMessage>("").await
+        self.get::<(), HaStatusMessage>("", None).await
     }
 
     pub async fn get_config(&self) -> Result<HaConfig, HaError> {
-        self.get::<HaConfig>(CONFIG).await
+        self.get::<(), HaConfig>(CONFIG, None).await
+    }
+
+    pub async fn get_components(&self) -> Result<Components, HaError> {
+        self.get::<(), Components>(COMPONENTS, None).await
+    }
+
+    pub async fn get_events(&self) -> Result<Events, HaError> {
+        self.get::<(), Events>(EVENTS, None).await
+    }
+
+    pub async fn get_services(&self) -> Result<Services, HaError> {
+        self.get::<(), Services>(SERVICES, None).await
+    }
+
+    pub async fn get_history(&self, timestamp: Option<DateTime<Utc>>, query: Option<&HistoryOptions>, entity_ids: Vec<String>) -> Result<HistoryResponse, HaError> {
+        if entity_ids.is_empty() {
+            return Err(HaError::MissingEntityId)
+        }
+
+        let query_params = if let Some(query) = query { 
+            HistoryQuery::from_query_options(query, entity_ids)
+        } else {
+            HistoryQuery::from_default(entity_ids)
+        };
+
+        let mut path = HISTORY.to_owned();
+        if let Some(timestamp) = timestamp {
+            path = format!("{path}/{}", timestamp);
+        }
+
+        self.get::<HistoryQuery, HistoryResponse>(&path, Some(&query_params)).await
+    }
+
+    pub async fn get_logbook(&self, timestamp: Option<DateTime<Utc>>, query: Option<&LogbookOptions>) -> Result<LogbookResponse, HaError> {
+        let mut path = LOGBOOK.to_owned();
+        if let Some(timestamp) = timestamp {
+            path = format!("{path}/{}", timestamp);
+        }
+
+        self.get::<LogbookOptions, LogbookResponse>(&path, query).await
+    }
+
+    pub async fn get_states(&self) -> Result<StatesResponse, HaError> {
+        self.get::<(), StatesResponse>(STATES, None).await
+    }
+
+    pub async fn get_entity_state(&self, entity_id: String) -> Result<StateObject, HaError> {
+        let path = format!("{}/{}", STATES, entity_id);
+        self.get::<(), StateObject>(&path, None).await
     }
 }
 
@@ -35,12 +85,16 @@ impl HaRestClient {
         format!("{}/api/{}", self.base_url, path)
     }
 
-    async fn get<T: DeserializeOwned>(&self, path: &str) -> Result<T, HaError> {
-        let response = self.client
+    async fn get<Q: Serialize, T: DeserializeOwned>(&self, path: &str, query: Option<&Q>) -> Result<T, HaError> {
+        let mut request = self.client
             .get(self.url(path))
-            .bearer_auth(&self.token)
-            .send()
-            .await?;
+            .bearer_auth(&self.token);
+
+        if let Some(query) = query {
+            request = request.query(query);
+        }
+
+        let response = request.send().await?;
 
         Self::handle_response(response).await
     }
