d8736c66aa4f0d727592b889f0e0c6ab44615b4d
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-16 14:10:29 +0200

    feat: updated initial command parsing and client creation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
 mod cli;
-mod ha;
 mod config;
+mod ha;
 
 use clap::Parser;
 use cli::Cli;
 
-use crate::{cli::error::AppError, config::{ConfigService, secret::KeyringSecretStorage, storage::{ConfigStorage, FsConfigStorage}}};
+use crate::{
+    cli::error::AppError,
+    config::{ConfigService, secret::KeyringSecretStorage, storage::FsConfigStorage},
+    ha::HaClient,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
     let cli = Cli::parse();
-    println!("{:?}", cli.command);
-
     let config_manager = ConfigService::new(KeyringSecretStorage, FsConfigStorage);
+    if let Some(cli::Commands::Auth { command }) = cli.command {
+        return cli::auth::handle(command, &config_manager).await;
+    }
 
+    let ha = HaClient::from_config(&config_manager).await?;
     match cli.command {
-        Some(command) => {
-            match command {
-                cli::Commands::Auth { command } => cli::auth::handle(command, &config_manager).await?,
-                cli::Commands::Entities { command } => unreachable!(),
-            }
+        Some(command) => match command {
+            cli::Commands::Auth { command: _ } => unreachable!(),
+            cli::Commands::Config => cli::config::handle(&ha).await?,
         },
         None => unreachable!(),
     }
